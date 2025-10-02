f3d5b43f6295c25e864280ebb9b0784d1838120c
suciayu28 <suci24si@mahasiswa.pcr.ac.id>
2025-10-02 10:41:19 +0700

    menambahkan halaman respon form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 
@@ -28,3 +29,6 @@ Route::get('/about', function () {
 
 
 Route::get('/home',[HomeController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
